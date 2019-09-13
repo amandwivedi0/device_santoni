51a447c000c06bb031332402bf2c210e658a28d6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-13 12:58:40 +0000

    santoni: Dexpreopt everything

--- device.mk
+++ device.mk
@@ -188,11 +188,6 @@ PRODUCT_PACKAGES += \
 # Charger
 include $(LOCAL_PATH)/rootdir/charger/charger.mk
 
-# Dex
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
-    NexusLauncherRelease
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8937 \
