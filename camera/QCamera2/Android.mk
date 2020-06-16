9cca78d76607aab8cb949023283bc4ce66ea4c33
FranticCoin <nikhilbaddi98@gmail.com>
2020-06-16 10:09:16 +0000

    santoni: Add missing header path maps

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -122,7 +122,11 @@ LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display-caf-msm8996/libqservice
+        hardware/qcom/display-caf-msm8996/libqservice \
+	hardware/qcom/display-caf-msm8996/libqdutils \
+	hardware/qcom/display-caf-msm8996/gralloc \
+	hardware/qcom/display-caf-msm8996/include
+
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
